42cd081a6942c39eb287fac824ea332f483a321b
Lucie Ladrova <lucie.ladrova@puxdesign.cz>
2019-12-01 15:19:54 +0100

    homepage, navigation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+        <div class="nav-box">
+        <div class="nav-item-box">
+            <a href="" class="nav-item">
+              <h2>Cviky</h2>
+              <img src="../assets/001-kettlebell.svg" alt="ikona kettlebellu">
+            </a>
+          </div>
+      
+          <div class="nav-item-box">
+            <a href="" class="nav-item">
+              <h2>Nový trénink</h2>
+              <img src="../assets/002-add.svg" alt="ikona přidání tréninku">
+            </a>
+          </div>
+          <div class="nav-item-box">
+            <a href="" class="nav-item">
+              <h2>Historie</h2>
+              <img src="../assets/003-clock-circular-outline.svg" alt="ikona historie tréninku">
+            </a>
+          </div>
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "../scss/_basics.scss";
+.nav-box {
+    margin: 0 -1.5rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    @include media(md) {
+        flex-wrap: nowrap;
+    }
+}
+.nav-item-box {
+    flex: 0 1 40rem;
+    padding: 1.5rem;
+
+    @include media (md) {
+        flex: 1 1 33.3333%;
+        max-width: auto;
+    }
+}
+.nav-item {
+    display: block;
+    text-align: center;
+    background-color: $main-color-light;
+    padding: 5rem;
+    border-radius: 5px;
+    text-decoration: none;
+    img {
+        max-width: 15rem;
+    }
+    h2 {
+        color: $base-white;
+    }
+}
+
+</style>
---
+++ src/views/Navigation.vue
@@ -0,0 +1,95 @@
+<template>
+  <header >
+
+        <nav>
+          <div class="hamburger-box" @click='openMenu = !openMenu'>
+              <div class="hamburger"></div>
+          </div>
+       
+            <ul class="menu" v-bind:class="{'open' : openMenu}">
+              <li><a href="">Cviky</a></li>
+              <li><a href="">Nový trénink</a></li>
+              <li><a href="">Historie</a></li>
+            </ul>
+          </nav>
+
+  </header>
+
+  
+</template>
+
+<script>
+export default {
+  data: function () {
+  return {
+    openMenu: false
+  }
+},
+  name: 'navigation',    
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/_basics.scss";
+nav {
+  margin: 0;
+  padding: 1.5rem 1.5rem 1.5rem 1.5rem;
+  margin: 0 0 1.5rem 0;
+  background-color: $main-color-light;
+}
+
+.menu {
+  list-style: none;
+  margin: 3rem 0 1.5rem 0;
+  padding: 0;
+  display: none;
+  li {
+    background-color: rgba(255, 255, 255, 0.3);
+    margin: 1rem 0;
+    padding: 0.5rem;
+  }
+  a {
+    text-decoration: none;
+    color: $base-black;
+    font-weight: 700;
+    font-size: 2rem;
+    text-transform: uppercase;
+  }
+  &.open {
+    display: block;
+  }
+}
+
+.hamburger {
+  display: block;
+  height: 4px;
+  width: 30px;
+  background-color: $base-black;
+  z-index: 99;
+  &:before {
+    content: '';
+    display: block;
+    height: 4px;
+    width: 30px;
+    background-color: $base-black;
+    transform: translate(0, 7px);
+  }
+  &:after {
+    content: '';
+    display: block;
+    height: 4px;
+    width: 30px;
+    background-color: $base-black;
+    transform: translate(0, -11px);
+  }
+  &-box {
+    cursor: pointer;
+    padding: 0.5rem;
+    width: 30px;
+    margin: 0.5rem 0;
+  }
+}
+
+</style>
